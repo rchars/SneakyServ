8f2881aa50e656841e77345ef85270c08e5423bf
rchars <rchars@protonmail.com>
2024-08-24 13:55:34 +0200

    Bug fixes

--- sneakyserv/sneakyserv.py
+++ sneakyserv/sneakyserv.py
@@ -1,4 +1,4 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, wait, FIRST_COMPLETED
 import threading
 import argparse
 import socket
@@ -12,8 +12,6 @@ class HandleClients(threading.Thread):
         self._setup_conns_hook = hook
         self._sock = sock
         self._args = args
-
-    def _setup_conns(self): return self._setup_conns_hook(self)
 
     def _handle_client(self, local_sock, remote_sock):
         t_1 = threading.Thread(target=self._send_and_recv, args=(local_sock, remote_sock,))
@@ -32,27 +30,34 @@ class HandleClients(threading.Thread):
             except OSError: pass
 
     def run(self):
-        try: self._setup_conns()
+        try: self._setup_conns_hook(self)
         finally: self.close_all_socks()
 
 def _recv_mode(inst):
     inst._remote_client_sock = inst._sock
-    passw_count = len(inst._args.passw)
-    bytes_left = 255
-    passw = ''
-    while (chunk := inst._remote_client_sock.recv(bytes_left).decode()) and bytes_left != 0:
-        if chunk == '': return
-        bytes_left -= len(chunk)
-        eol_index = chunk.find('\n')
-        if eol_index != -1:
-            if passw + chunk[0:eol_index] == inst._args.passw:
-                inst._local_client_sock = socket.create_connection((inst._args.rhost, inst._args.rport), timeout=inst._args.rhost_timeout)
-                inst._local_client_sock.send(chunk[eol_index + 1:-1].encode())
-                inst._handle_client(inst._local_client_sock, inst._remote_client_sock)
-                return
-            else: return
-        passw += chunk
-    return
+    is_authed = threading.Event()
+    def auth():
+        passw_count = len(inst._args.passw)
+        bytes_left = 255
+        passw = ''
+        while (chunk := inst._remote_client_sock.recv(bytes_left).decode()) and bytes_left != 0:
+            if chunk == '': return
+            bytes_left -= len(chunk)
+            eol_index = chunk.find('\n')
+            if eol_index != -1:
+                if passw + chunk[0:eol_index + 1] == inst._args.passw:
+                    inst._local_client_sock = socket.create_connection((inst._args.rhost, inst._args.rport), timeout=inst._args.rhost_timeout)
+                    inst._local_client_sock.send(chunk[eol_index + 1:-1].encode())
+                    is_authed.set()
+                    return
+                else: return
+            passw += chunk
+    auth_thread = threading.Thread(target=auth)
+    auth_thread.start()
+    auth_thread.join(inst._args.auth_timeout)
+    if not auth_thread.is_alive() and is_authed.is_set():
+        inst._remote_client_sock.settimeout(inst._args.rhost_timeout)
+        inst._handle_client(inst._local_client_sock, inst._remote_client_sock)
 
 def _send_mode(inst):
         inst._local_client_sock = inst._sock
@@ -63,15 +68,23 @@ def _send_mode(inst):
 # TODO:
     # Prettify main
 def main():
+    def __passw(passw):
+        eol_count = passw.count('\n')
+        if eol_count > 1: raise argparse.ArgumentTypeError('Passw may contain at most one newline character, and it must be at the end.')
+        elif not passw.endswith('\n'): passw += '\n'
+        return passw
+
     parser = argparse.ArgumentParser(description='SneakySSH - Obfuscator for SSH connections.')
     parser.add_argument('--rhost', '-r', required=True, help='The remote IP address to connect to.')
     parser.add_argument('--lhost', '-l', required=True, help='The IP address to listen on.')
-    parser.add_argument('--passw', '-p', required=True, help='The banner/password to send/receive (depending on the mode).')
+    parser.add_argument('--passw', '-p', required=True, type=__passw, help='The banner/password to send/receive (depending on the mode).')
 
     default_timeout = socket.getdefaulttimeout()
     parser.add_argument('--lhost-timeout', dest='lhost_timeout', type=float, default=default_timeout, help='The timeout for incoming connections (in seconds).')
     parser.add_argument('--rhost-timeout', dest='rhost_timeout', type=float, default=default_timeout, help='The timeout for connections to the remote server (in seconds).')
+
     parser.add_argument('--client-limit', dest='client_limit', type=int, default=3, help='The maximum number of clients that can be accepted.')
+    parser. add_argument('--auth-timeout', dest='auth_timeout', type=float, default=2.5, help='The authentication timeout (in seconds).')
 
     parser.add_argument('--rport', '--rp', type=int, default=9999, help='The port of the remote server.')
     parser.add_argument('--lport', '--lp', type=int, default=9999, help='The port to listen on.')
@@ -88,10 +101,9 @@ def main():
         # Add secret passw
 
     args = parser.parse_args()
-
     with ThreadPoolExecutor(max_workers=args.client_limit) as executor:
-        futures = []
         client_handlers = []
+        futures = []
         serv_sock = None
         hook = None
         if args.recv: hook = _recv_mode
@@ -105,18 +117,19 @@ def main():
                 client_handler = HandleClients(args, sock, hook)
                 client_handlers.append(client_handler)
                 futures.append(executor.submit(client_handler.run))
-                if len(futures) != args.client_limit: continue
-                for future in as_completed(futures):
-                    result = future.result()
+                if len(futures) >= args.client_limit:
+                    wait(futures, return_when=FIRST_COMPLETED)
+                for future in futures:
+                    if not future.done(): continue
                     i = futures.index(future)
-                    futures.pop(i)
                     client_handlers.pop(i)
-        except (KeyboardInterrupt, EOFError):
-            for client_handler in client_handlers:
-                client_handler.close_all_socks()
-            print()
-            sys.exit(120)
+                    futures.pop(i)
+        except (KeyboardInterrupt, EOFError): print()
         finally:
             if serv_sock is not None: serv_sock.close()
+            for client_handler in client_handlers:
+                client_handler.close_all_socks()
+            for future in futures: future.cancel()
+            sys.exit(120)
 
 if __name__ == '__main__': main()
