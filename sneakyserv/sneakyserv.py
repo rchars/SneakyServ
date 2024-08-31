662c72b7c229760c07cba92d4f1e58e285db0206
rchars <rchars@protonmail.com>
2024-08-31 15:45:03 +0200

    Bug fixes

--- sneakyserv/sneakyserv.py
+++ sneakyserv/sneakyserv.py
@@ -1,73 +1,114 @@
 from concurrent.futures import FIRST_EXCEPTION, ThreadPoolExecutor, wait, FIRST_COMPLETED
-import threading
+from abc import ABC, abstractmethod, abstractproperty
+from collections import namedtuple
+from datetime import datetime
 import argparse
 import socket
 import sys
 
-class HandleClients(threading.Thread):
-    def __init__(self, args, sock, hook):
-        super().__init__()
-        self._remote_client_sock = None
-        self._local_client_sock = None
-        self._setup_conns_hook = hook
-        self._sock = sock
-        self._args = args
+class HandleClients(ABC):
+    ClientInfo = namedtuple('ClientInfo', ['cli_type', 'sock', 'info'])
 
-    def _handle_client(self, local_sock, remote_sock):
+    def __init__(self, args):
+        self.first_cli_type = None
+        self.socks_info = list()
+        self.running = False
+        self.args = args
+
+    @abstractmethod
+    def setup_conns(self): pass
+
+    def print_status(self, client_info, msg):
+        formatted_current_time = datetime.now().strftime('%H:%M:%S')
+        print(f'[{formatted_current_time}][{client_info.cli_type}] => {hex(id((client_info.sock)))} => {client_info.info[0]}:{client_info.info[1]} {msg}')
+
+    def handle_client(self, local_sock, remote_sock):
         executor = ThreadPoolExecutor(max_workers=2)
         futures = (
-            executor.submit(self._send_and_recv, local_sock, remote_sock),
-            executor.submit(self._send_and_recv, remote_sock, local_sock)
+            executor.submit(self.send_and_recv, local_sock, remote_sock),
+            executor.submit(self.send_and_recv, remote_sock, local_sock)
         )
         wait(futures, return_when=FIRST_COMPLETED)
         executor.shutdown(wait=False)
 
-    def _send_and_recv(self, send_sock, recv_sock):
-        while (data := recv_sock.recv(self._args.buff_size)) and data != b'': send_sock.send(data)
+    def send_and_recv(self, send_sock, recv_sock):
+        while (data := recv_sock.recv(self.args.buff_size)) and data != b'': send_sock.send(data)
 
     def close_all_socks(self):
-        for sock in (self._remote_client_sock, self._local_client_sock):
+        for client_info in self.socks_info:
             try:
-                if sock is not None: sock.shutdown(socket.SHUT_RDWR)
+                if client_info.sock is None: continue
+                client_info.sock.shutdown(socket.SHUT_RDWR)
+                self.print_status(client_info, 'disconnected')
             except OSError: pass
 
+    def request_shutdown(self):
+        if self.running: self.close_all_socks()
+
     def run(self):
-        try: self._setup_conns_hook(self)
-        finally: self.close_all_socks()
+        try:
+            self.running = True
+            self.setup_conns()
+        finally:
+            self.running = False
+            self.close_all_socks()
 
-# TODO:
-    #  Use concurrent package instead of threading module
-def _recv_mode(inst):
-    inst._remote_client_sock = inst._sock
-    is_authed = threading.Event()
-    def auth():
-        passw_count = len(inst._args.passw)
-        bytes_left = 255
-        passw = ''
-        while (chunk := inst._remote_client_sock.recv(bytes_left).decode()) and bytes_left != 0:
-            if chunk == '': return
-            bytes_left -= len(chunk)
-            eol_index = chunk.find('\n')
-            if eol_index != -1:
-                if passw + chunk[0:eol_index + 1] == inst._args.passw:
-                    inst._local_client_sock = socket.create_connection((inst._args.rhost, inst._args.rport), timeout=inst._args.rhost_timeout)
-                    inst._local_client_sock.send(chunk[eol_index + 1:-1].encode())
-                    is_authed.set()
-                    return
-                else: return
-            passw += chunk
-    auth_thread = threading.Thread(target=auth)
-    auth_thread.start()
-    auth_thread.join(inst._args.auth_timeout)
-    if not auth_thread.is_alive() and is_authed.is_set():
-        inst._remote_client_sock.settimeout(inst._args.rhost_timeout)
-        inst._handle_client(inst._local_client_sock, inst._remote_client_sock)
+    def handle_clients(self, serv_sock):
+        self.sock, self.sock_info = serv_sock.accept()
+        client_info = self.ClientInfo(sock=self.sock, info=self.sock_info, cli_type=self.first_cli_type)
+        self.print_status(client_info, 'connected')
+        self.socks_info.append(client_info)
+        executor = ThreadPoolExecutor(max_workers=1)
+        future = executor.submit(self.run)
+        executor.shutdown(wait=False, cancel_futures=False)
+        return future
 
-def _send_mode(inst):
-        inst._local_client_sock = inst._sock
-        inst._remote_client_sock = socket.create_connection((inst._args.rhost, inst._args.rport), timeout=inst._args.rhost_timeout)
-        inst._remote_client_sock.send(inst._args.passw.encode())
-        inst._handle_client(inst._local_client_sock, inst._remote_client_sock)
+class RecvMode(HandleClients):
+    def __init__(self, args):
+        super().__init__(args)
+        self.first_cli_type = 'remote'
+
+    def setup_conns(self):
+        remote_client_sock = self.sock
+        def auth():
+            passw_count = len(self.args.passw)
+            bytes_left = 255
+            passw = ''
+            while (chunk := remote_client_sock.recv(bytes_left).decode()) and bytes_left != 0:
+                if chunk == '': return
+                bytes_left -= len(chunk)
+                eol_index = chunk.find('\n')
+                if eol_index != -1:
+                    if passw + chunk[0:eol_index + 1] == self.args.passw:
+                        local_client_sock = socket.create_connection((self.args.rhost, self.args.rport), timeout=self.args.rhost_timeout)
+                        client_info = self.ClientInfo(sock=local_client_sock, info=local_client_sock.getsockname(), cli_type='local')
+                        self.socks_info.append(client_info)
+                        self.print_status(client_info, 'connected')
+                        local_client_sock.send(chunk[eol_index + 1:-1].encode())
+                        return local_client_sock
+                    else: return
+                passw += chunk
+        executor = ThreadPoolExecutor(max_workers=1)
+        auth_worker = executor.submit(auth)
+        try: local_client_sock = auth_worker.result(timeout=self.args.auth_timeout)
+        except TimeoutError: pass
+        else:
+            remote_client_sock.settimeout(self.args.rhost_timeout)
+            self.handle_client(local_client_sock, remote_client_sock)
+
+class SendMode(HandleClients):
+    def __init__(self, args):
+        super().__init__(args)
+        self.first_cli_type = 'local'
+
+    def setup_conns(self):
+        local_client_sock = self.sock
+        remote_client_sock = socket.create_connection((self.args.rhost, self.args.rport), timeout=self.args.rhost_timeout)
+        client_info = self.ClientInfo(sock=remote_client_sock, info=remote_client_sock.getsockname(), cli_type='remote')
+        self.socks_info.append(client_info)
+        self.print_status(client_info, 'connected')
+        remote_client_sock.send(self.args.passw.encode())
+        self.handle_client(local_client_sock, remote_client_sock)
 
 # TODO:
     # Prettify main
@@ -88,7 +129,7 @@ def main():
     parser.add_argument('--rhost-timeout', dest='rhost_timeout', type=float, default=default_timeout, help='The timeout for connections to the remote server (in seconds).')
 
     parser.add_argument('--client-limit', dest='client_limit', type=int, default=3, help='The maximum number of clients that can be accepted.')
-    parser. add_argument('--auth-timeout', dest='auth_timeout', type=float, default=2.5, help='The authentication timeout (in seconds).')
+    parser.add_argument('--auth-timeout', dest='auth_timeout', type=float, default=2.5, help='The authentication timeout (in seconds).')
 
     parser.add_argument('--rport', '--rp', type=int, default=9999, help='The port of the remote server.')
     parser.add_argument('--lport', '--lp', type=int, default=9999, help='The port to listen on.')
@@ -107,36 +148,32 @@ def main():
         # Add secret passw
 
     args = parser.parse_args()
+    if args.recv: client_handler_class = RecvMode
+    else: client_handler_class = SendMode
+
     with ThreadPoolExecutor(max_workers=args.client_limit) as executor:
         client_handlers = []
         futures = []
         serv_sock = None
-        hook = None
-        if args.recv: hook = _recv_mode
-        else: hook = _send_mode
         try:
             serv_sock = socket.create_server((args.lhost, args.lport), reuse_port=True)
             serv_sock.settimeout(args.lhost_timeout)
             serv_sock.listen()
             while True:
-                sock = serv_sock.accept()[0]
-                client_handler = HandleClients(args, sock, hook)
+                client_handler = client_handler_class(args)
                 client_handlers.append(client_handler)
-                futures.append(executor.submit(client_handler.run))
+                futures.append(client_handler.handle_clients(serv_sock))
                 if len(futures) >= args.client_limit: wait(futures, return_when=FIRST_COMPLETED)
                 for future in futures:
                     if not future.done(): continue
                     i = futures.index(future)
-                    client_handlers[i].close_all_socks()
+                    client_handlers[i].request_shutdown()
                     client_handlers.pop(i)
-                    futures[i].cancel()
                     futures.pop(i)
         except (KeyboardInterrupt, EOFError): print()
         finally:
             if serv_sock is not None: serv_sock.close()
-            for client_handler in client_handlers:
-                client_handler.close_all_socks()
-            for future in futures: future.cancel()
+            for client_handler in client_handlers: client_handler.request_shutdown()
             sys.exit(120)
 
 if __name__ == '__main__': main()
